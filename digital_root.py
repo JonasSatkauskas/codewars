1ac09aba5b327ba2bdb67ae64b8640191b9b9453
Joe <joncius224@gmail.com>
2021-05-29 19:45:43 +0300

    Done!

--- digital_root.py
+++ digital_root.py
@@ -10,35 +10,26 @@ def digital_root(n):
     skaiciu_listas = [int(d) for d in str(n)]
     for skaicius in range(0, len(skaiciu_listas)):
         suma = suma + skaiciu_listas[skaicius]
-
+   
+    if len(str(suma)) > 1:
+        final = 0
+        final_sarasas = [int(b) for b in str(suma)]
+        for num in range(0, len(final_sarasas)):
+            final = final + final_sarasas[num]
+        if len(str(final)) > 1:
+            galutine = 0
+            sarasas = [int(b) for b in str(final)]
+            for number in range(0, len(sarasas)):
+                galutine = galutine + sarasas[number]
+            return galutine
+    
     if len(str(suma)) > 1:
         galutine = 0
         sarasas = [int(b) for b in str(suma)]
-
         for number in range(0, len(sarasas)):
             galutine = galutine + sarasas[number]
         return galutine
-
     else:
         return suma
 
-
-
-
-
-
-print(digital_root(493193))
-
-
-# suma = 0
-
-# sarasas = [1,2,3,4,5]
-
-# for num in range(0, len(sarasas)):
-#     suma = suma + sarasas[num]
-
-# print(suma)
-
-
-
-
+print(digital_root(623))
