37295bc9ef87433d39dce84f08fd4c0e7220e525
Joe <joncius224@gmail.com>
2021-06-02 18:44:16 +0300

    Almost done

--- X_and_O.py
+++ X_and_O.py
@@ -1,2 +1,23 @@
 '''Check to see if a string has the same amount of 'x's and 'o's. 
 The method must return a boolean and be case insensitive. The string can contain any char.'''
+
+import re
+
+def xo(s):
+    # Finding x's in the given string
+    x_findings = re.findall('x', s)
+    # Counting how many 'x' letters there are
+    x_count = len(x_findings)
+
+    # Finding o's in the given string
+    o_findings = re.findall('o', s)
+    # Counting how many 'o' letters there are
+    o_count = len(o_findings)
+
+    # Checking final condition
+    if x_count == o_count:
+        return True
+    else:
+        return False
+
+print(xo('xooxx'))
