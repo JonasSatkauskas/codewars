32eddd810d3abda215d6887bd56579eda330cc89
JonasSatkauskas <satkusmedia@gmail.com>
2021-06-20 10:37:54 +0300

    Almost done

--- Regex_PIN.py
+++ Regex_PIN.py
@@ -4,8 +4,16 @@
 and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 If the function is passed a valid PIN string, return true, else return false.'''
 
+import re
+
 
 def validate_pin(pin):
     #return true or false
+    four_digits_check = re.search("^\d{4}$", str(pin))
+    six_digits_check = re.search("^\d{6}$", str(pin))
 
-    # FOR GIT PUR{OSES}
+    if four_digits_check or six_digits_check:
+        return True
+    else:
+        return False    
+    
