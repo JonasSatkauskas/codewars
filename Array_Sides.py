6b277a35c51674827cf8b374cd01ca361399f435
JonasSatkauskas <satkusmedia@gmail.com>
2021-07-04 16:21:31 +0300

    Files creation

---
+++ Array_Sides.py
@@ -0,0 +1,15 @@
+"""You are going to be given an array of integers.
+ Your job is to take that array and find an index X
+ where the sum of the integers to the left of X is equal to the sum of the integers to the right
+of X. If there is no index that would make this happen, return -1.
+
+For example:
+
+Let's say you are given the array {1,2,3,4,3,2,1}:
+Your function will return the index 3, because at the 3rd position of the array, 
+the sum of left side of the index ({1,2,3})
+ and the sum of the right side of the index ({3,2,1}) both equal 6."""
+
+
+ def find_even_index(arr):
+    #your code here
