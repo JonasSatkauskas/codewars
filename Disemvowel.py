154baa72a590936d93f025144c49b8db1d30214c
JonasSatkauskas <satkusmedia@gmail.com>
2021-07-19 03:02:15 +0300

    Finished task

--- Disemvowel.py
+++ Disemvowel.py
@@ -4,4 +4,12 @@ For example, the string "This website is for losers LOL!" would become
 "Ths wbst s fr lsrs LL!".
 Note: for this kata y isn't considered a vowel.
 """
+import re
 
+
+def disemvowel(string_):
+    patterm = r'[aeiouAEIOU]'
+    modified_string = re.sub(patterm, '', string_)
+    return modified_string
+
+print(disemvowel('I will try to make it easy'))
