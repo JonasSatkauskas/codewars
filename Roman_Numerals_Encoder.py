d1a2a4603001bfe6643c1041da794c854817fdf0
JonasSatkauskas <satkusmedia@gmail.com>
2021-08-08 21:24:37 +0300

    Finished task

--- Roman_Numerals_Encoder.py
+++ Roman_Numerals_Encoder.py
@@ -6,3 +6,29 @@ starting with the left most digit and skipping any digit with a value of zero.
 In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 
 2008 is written as 2000=MM, 8=VIII; or MMVIII.
 1666 uses each Roman symbol in descending order: MDCLXVI."""
+
+
+def solution(n):
+    # TODO convert int to roman string
+    val = [
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+            ]
+    syb = [
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+            ]
+    roman_num = ''
+    i = 0
+    while  n > 0:
+        for _ in range(n // val[i]):
+            roman_num += syb[i]
+            n -= val[i]
+        i += 1
+    return roman_num
+
+print(solution(55))
