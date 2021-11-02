2707e576e6927d30c3f900cf42e7770741fabc39
JonasSatkauskas <satkusmedia@gmail.com>
2021-11-02 14:52:52 +0200

    Done

--- Middle_Character.py
+++ Middle_Character.py
@@ -8,4 +8,13 @@ If the word's length is even, return the middle 2 characters.
 
 
 def get_middle(word):
-    pass
+    if len(word) % 2 == 0:
+        offset = int(len(word) / 2)
+        return word[offset - 1 : offset + 1]
+    else:
+        offset = int(len(word) / 2)
+        return word[offset : offset + 1]
+
+
+print(get_middle("sveika"))
+print(get_middle("sveikas"))
